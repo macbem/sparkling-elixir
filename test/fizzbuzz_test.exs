56bcd1bc93c20db0f102ad3d608ea676afebd4c6
Maciej Bembenista <maciej.bembenista@gmail.com>
2017-12-09 00:29:22 +0100

    Remove random values in test cases

--- test/fizzbuzz_test.exs
+++ test/fizzbuzz_test.exs
@@ -3,66 +3,30 @@ defmodule FizzBuzzTest do
   use ExUnit.Case, async: true # tracing is enabled, so only one task is running at any given moment
   doctest FizzBuzz
 
-  defp get_random_test_value(divisor), do: :rand.uniform(9999999) * divisor
-  defp get_random_test_value(), do: :rand.uniform(9999999)
-
   test "concats two lists properly" do
     assert Exercise.list_concat [:a, :b], [:c, :d] == [:a, :b, :c, :d]
   end
 
   test "returns a correct value for 15-divisible entry" do
-    test_input = 15
-      |> get_random_test_value
-      |> FizzBuzz.get_single_value
+    assert FizzBuzz.get_single_value(15) ==  "FizzBuzz"
 
-    assert test_input ==  "FizzBuzz"
-
-    test_input = get_random_test_value(15) + 1
-      |> get_random_test_value
-      |> FizzBuzz.get_single_value
-
-    refute test_input ==  "FizzBuzz"
+    refute FizzBuzz.get_single_value(15 + 1) == "FizzBuzz"
   end
 
   test "returns a correct value for 5-divisible entry" do
-    test_input = 5
-      |> get_random_test_value
-      |> FizzBuzz.get_single_value
+    assert FizzBuzz.get_single_value(5) == "Buzz"
 
-    assert test_input ==  "Buzz"
-
-    test_input = get_random_test_value(5) + 1
-      |> get_random_test_value
-      |> FizzBuzz.get_single_value
-
-    refute test_input ==  "Buzz"
+    refute FizzBuzz.get_single_value(5 + 1) == "Buzz"
   end
 
   test "returns a correct value for 3-divisible entry" do
-    test_input = 3
-      |> get_random_test_value
-      |> FizzBuzz.get_single_value
+    assert FizzBuzz.get_single_value(3) == "Fizz"
 
-    assert test_input ==  "Fizz"
-
-    test_input = get_random_test_value(3) + 1
-      |> get_random_test_value
-      |> FizzBuzz.get_single_value
-
-    refute test_input ==  "Fizz"
+    refute FizzBuzz.get_single_value(3 + 1) == "Fizz"
   end
 
   test "returns a correct value for entry non divisible by 3 and/or 5 " do
-    test_input = get_random_test_value()
-
-    test_input =
-      case test_input do
-        test_input when rem(test_input, 5) == 0 -> test_input + 1
-        test_input when rem(test_input, 3) == 0 -> test_input + 1
-        _ -> test_input
-      end
-
-    assert test_input == FizzBuzz.get_single_value test_input
+    assert 1 == FizzBuzz.get_single_value 1
   end
 
 end
