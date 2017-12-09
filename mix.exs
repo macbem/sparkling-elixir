c7d796df943598cc891ab86628a052a4d9b52547
macbem <maciej.bembenista@gmail.com>
2017-12-09 01:55:39 +0100

    Move the docs to the 'docs' folder

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule ExploringElixir.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: [
+        output: "docs",
+        extras: ["README.md"]
+      ]
     ]
   end
 
